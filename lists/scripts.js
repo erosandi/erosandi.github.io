dd45032fcf5dedbf122923bb7135864fa68a8185
erosandi <63194734+erosandi@users.noreply.github.com>
2020-04-21 15:17:38 +0200

    Create scripts.js

---
+++ lists/scripts.js
@@ -0,0 +1,17 @@
+let names = ["Berni", "Emil", "Andi", "Bertold"];
+
+names.forEach((names) =>
+{
+    $("ul").append(`<li> ${names} </li>`);
+});
+
+
+
+  $('body').append(
+    '<h1>Added with javascript</h1>'
+    
+  );
+
+  $('body').append(
+    `<p>This block was added using JavaScript's jQuery library. How awesome!</p>`
+  );
